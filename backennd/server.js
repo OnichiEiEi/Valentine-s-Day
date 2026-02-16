cb2220443df6215f3559ebcd1de4f0b0c9eac864
OnichiEiEi <mongri2244@gmail.com>
2026-02-17 06:39:59 +0700

    0.0.5 fix bug

--- backennd/server.js
+++ backennd/server.js
@@ -13,7 +13,7 @@ connectDB();
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cors({
-    origin: 'process.env.FRONTEND_URL',
+    origin: process.env.FRONTEND_URL,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
