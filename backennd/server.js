32c3498d230d5674d7844cd49d3d703b12733e3d
OnichiEiEi <mongri2244@gmail.com>
2026-02-17 22:16:40 +0700

    0.0.8 fix cors for vercle

--- backennd/server.js
+++ backennd/server.js
@@ -5,6 +5,7 @@ import { connectDB } from './configs/mongo.js'
 import dotenv from "dotenv";
 
 const PORT = process.env.PORT || 5000;
+const vercelRegex = /^https:\/\/.*\.vercel\.app$/;
 
 dotenv.config();
 const app = express();
@@ -13,7 +14,7 @@ connectDB();
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
+    origin: vercelRegex,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
